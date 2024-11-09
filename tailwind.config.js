68b0a954d100d976ba9005166783d7504c9f77b8
giova0412 <230314@utxicotepec.edu.mx>
2024-11-09 15:00:15 -0600

    practica 12
    se actualizo la paleta de colores

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,17 @@
 export default {
   content: ['./views/**/*.pug'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#C3f73a',          // Verde claro
+        secondary: '#68b684',        // Verde oscuro
+        accent: '#094d92',           // Azul
+        neutral: '#000000',          // Negro
+        background: '#ffffff',       // Blanco
+      },
+    },
   },
   plugins: [],
 }
+
 
